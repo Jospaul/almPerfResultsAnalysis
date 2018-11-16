6c64cb451ccd533c1e36a508c37af2d4924d02df
Jospaul Mahajan Prakash <jprakash@jcp.com>
2018-11-16 11:28:12 -0600

    Added MPOS Analysis files to new branch

---
+++ tcoe-performance-pos/ResultAnalysis/executeMPOSReport.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Author: Jospaul Mahajan Prakash
+REM Company: Infosys Limited
+
+set ALMUSERNAME=jprakash
+set ALMPASSWORD=Infosys}123
+
+REM Takes "file" for file only output, "teams" for teams only output and "tf" for both
+set OUTPUTTYPE=tf
+
+REM Takes a run id if results of a specific run id has to be computed
+set RUNID=1941
+
+python almPerfResultsAnalysis.egg -u %ALMUSERNAME% -p %ALMPASSWORD% -o %OUTPUTTYPE%
