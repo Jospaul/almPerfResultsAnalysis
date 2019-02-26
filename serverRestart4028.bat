1529122e294b27103eb197f24e7110589e9b700f
Jospaul Mahajan Prakash <jprakash@jcp.com>
2019-02-26 17:09:20 -0600

    Password Updated

--- serverRestart4028.bat
+++ serverRestart4028.bat
@@ -1,7 +1,7 @@
 @echo off
 REM trigger server restart
 
-cmd /c "cmdkey /add:entlr005 /user:jprakash /pass:Infosys_123"
+cmd /c "cmdkey /add:entlr005 /user:jprakash /pass:Infosys?123"
 
 C:\pstools\PsExec.exe \\entlr005 cmd /c "C:\Users\jprakash\Documents\cleanup_prep.bat"
 
