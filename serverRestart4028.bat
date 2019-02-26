0dfc0409518ce57a818e48afd14c000c457cee12
Jospaul Mahajan Prakash <jprakash@jcp.com>
2019-02-26 14:42:14 -0600

    Added credential creation

--- serverRestart4028.bat
+++ serverRestart4028.bat
@@ -1,6 +1,10 @@
 @echo off
 REM trigger server restart
 
+cmdkey /add:entlr005 /user:jprakash /pass:Infosys_123
+
 C:\pstools\PsExec.exe \\entlr005 cmd /c "C:\Users\jprakash\Documents\cleanup_prep.bat"
+
+cmdkey /delete:entlr005 /user:jprakash
 
 :exit
