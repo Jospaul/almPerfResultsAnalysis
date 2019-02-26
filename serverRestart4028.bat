fc3f80ea0c99cec19b7f70f98ee5ed57a35c56d6
Jospaul Mahajan Prakash <jprakash@jcp.com>
2019-02-26 14:49:17 -0600

    Modified script

--- serverRestart4028.bat
+++ serverRestart4028.bat
@@ -1,10 +1,10 @@
 @echo off
 REM trigger server restart
 
-cmdkey /add:entlr005 /user:jprakash /pass:Infosys_123
+cmd /c "cmdkey /add:entlr005 /user:jprakash /pass:Infosys_123"
 
 C:\pstools\PsExec.exe \\entlr005 cmd /c "C:\Users\jprakash\Documents\cleanup_prep.bat"
 
-cmdkey /delete:entlr005 /user:jprakash
+cmd /c "cmdkey /delete:entlr005"
 
 :exit
