ae96dc7c1546cc34116e5aaf65f413b3fbc6ee67
Jospaul <jprakash@jcp.com>
2019-11-26 13:39:54 -0600

    updated the setup

---
+++ almPerfResultsAnalysis/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='almPerfResultsAnalysis',
+    version='',
+    packages=['SupportFiles'],
+    url='',
+    license='',
+    author='Jospaul Mahajan Prakash',
+    author_email='jprakash@jcp.com',
+    description=''
+)
--- executeMPOSReport.bat
+++ executeMPOSReport.bat
@@ -11,4 +11,12 @@ set OUTPUTTYPE=tf
 REM Takes a run id if results of a specific run id has to be computed
 set RUNID=1941
 
+cmd /c "cmdkey /add:enttools201 /user:%ALMUSERNAME% /pass:%ALMPASSWORD%"
+
+cmd /c "cmdkey /add:len-file /user:%ALMUSERNAME% /pass:%ALMPASSWORD%"
+
 python .\almPerfResultsAnalysis\__main__.py -u %ALMUSERNAME% -p %ALMPASSWORD% -o %OUTPUTTYPE%
+
+cmd /c "cmdkey /delete:len-file
+
+cmd /c "cmdkey /delete:enttools201"
